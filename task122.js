d3a9fdfaebefbe8890b697c3142025c6eab1ab25
kkpkenneth <kkp.kenneth@gmail.com>
2022-02-15 08:16:20 +0100

    1

--- task122.js
+++ task122.js
@@ -1,33 +1,77 @@
 let status = "stopped";
-const sports=["3x3 Basketball", "Alpine Skiing", "Snooker", "Archery", "Biathlon", "Billiard", "Artistic Swimming", "Bobsleigh", "Gaming (E-sport)", "Athletics", "Cross Country Skiing", "Bowling", "Badminton", "Curling", "Lawn bowling"                           ]
+const place=["Bedroom", "Living room", "Kitchen", "Bathroom", "Basement", "Attic", "Garden", "Swimming pool", "Rainforest", "Ocean", "Desert", "Grassland", "Savanna", "Taiga", "Tundra"]
+const game=["Special mission", "Walk the dog", "", "Watch a cowgirl movie", "Eat with a spoon", "Iron the clothes", "Order bento delivery", "Take a car ride", "Plant a lotus", "Do 69 sit ups or push ups", "Tidy up"]
+const time=["5 min", "10 min", "15 min", "20 min", "25 min", "30 min", "35 min", "40 min", "45 min", "50 min", "55 min", "60 min", "70 min", "80 min", "90 min", "100 min", "120 min" ]
 //Stopwatch function (logic to determine when to increment next value, etc.)
 function stopWatch(){
 //Display updated time values to user
-    let randomIndex=Math.floor(Math.random()*sports.length);
-    document.getElementById("display").innerHTML =("value",sports[randomIndex]);
+    let randomIndex=Math.floor(Math.random()*place.length);
+    document.getElementById("display").innerHTML =("value",place[randomIndex]);
 }
+
+function stopWatch2(){
+//Display updated time values to user
+    let randomIndex=Math.floor(Math.random()*game.length);
+    document.getElementById("display2").innerHTML =("value",game[randomIndex]);
+}
+
+function stopWatch3(){
+//Display updated time values to user
+    let randomIndex=Math.floor(Math.random()*time.length);
+    document.getElementById("display3").innerHTML =("value",time[randomIndex]);
+}
+
 function startStop(){
 
     if(status === "stopped"){
 
         interval = window.setInterval(stopWatch, 0);
-        document.getElementById("startStop").innerHTML = "Stop Generating";
+        document.getElementById("startStop").innerHTML = "Stop Touching";
         playAudio();
         status = "started";
     }
     else{
 
         window.clearInterval(interval);
-        document.getElementById("startStop").innerHTML = "Start Again";
-        pauseAudio();
+        document.getElementById("startStop").innerHTML = "Touch Again";
+        status = "stopped";
+    }
+} 
+
+function startStop2(){
+
+    if(status === "stopped"){
+
+        interval2 = window.setInterval(stopWatch2, 0);
+        document.getElementById("startStop2").innerHTML = "Stop Touching";
+        playAudio();
+        status = "started";
+    }
+    else{
+
+        window.clearInterval(interval2);
+        document.getElementById("startStop2").innerHTML = "Touch Again";
+        status = "stopped";
+    }
+} 
+
+function startStop3(){
+
+    if(status === "stopped"){
+
+        interval3 = window.setInterval(stopWatch3, 0);
+        document.getElementById("startStop3").innerHTML = "Stop Touching";
+        playAudio();
+        status = "started";
+    }
+    else{
+
+        window.clearInterval(interval3);
+        document.getElementById("startStop3").innerHTML = "Touch Again";
         status = "stopped";
     }
 } 
 function playAudio() { 
         var a = document.getElementById("myAudio");
-            a.play();
-        }
-function pauseAudio() { 
-        var a = document.getElementById("myAudio");
-            a.pause();
+           
         }
